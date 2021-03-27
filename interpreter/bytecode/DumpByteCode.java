2d55a1dd66f4dcc34cc8a6f0608ba965cc145d5e
Tony <thuang54@mail.ccsf.edu>
2021-03-26 21:33:34 -0700

    created DumpByteCode for extention

---
+++ interpreter/bytecode/DumpByteCode.java
@@ -0,0 +1,13 @@
+package interpreter.bytecode;
+
+public class DumpByteCode {
+    boolean dump = false;
+
+    public boolean isDump() {
+        return dump;
+    }
+
+    public void setDump(boolean dump) {
+        this.dump = dump;
+    }
+}
